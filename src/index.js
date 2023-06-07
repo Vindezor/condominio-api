6fe8b6eb847906512a9d171ab7f348b746969960
Richard <richarddavidrv13@gmail.com>
2023-06-06 22:23:02 -0400

    mvc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const { app } = require("./api/api");
+const express=require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+const userRouter = require('./routes/user_route');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(cors());
+app.use('/api', userRouter);
 
 app.listen(3000, () => console.log("listening on port 3000"));
